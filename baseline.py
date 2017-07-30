b95c4153c68f4396003b59098d3e57f7f4615fde
Akshay Kashyap <AKK@zurich.ibm.com>
2017-07-30 17:07:24 +0200

    Use Reuters Dataset

--- baseline.py
+++ baseline.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import numpy as np
 
-from keras.datasets import imdb
+from keras.datasets import imdb, reuters
 
 import random
+
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 def pad_sequences(sequences, padded_len):
     return np.array([zero_pad(sequence, padded_len) for sequence in sequences])
@@ -15,26 +18,34 @@ def gen_batch(x, y, n_seq):
     indices = random.sample(range(len(x)), n_seq)
     return x[indices], y[indices]
 
+def one_hot(n, dim):
+    vec = [0] * dim
+    vec[n] = 1
+
+    return np.array(vec, dtype='f')
+
 # Load Data
-(x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz")
+# (x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz")
+(x_train, y_train), (x_test, y_test) = reuters.load_data(path="reuters.npz")
 
 # Pre-process
-MAX_LEN = len(max(x_train + x_test, key=len))
+MAX_LEN = max(len(max(x_train, key=len)), len(max(x_test, key=len)))
+N_CLASSES = 46
 
 x_train = pad_sequences(x_train, MAX_LEN)
 x_test = pad_sequences(x_test, MAX_LEN)
-y_train = np.array([[1 & int(not y), 1 & y] for y in y_train], dtype='f')
-y_test  = np.array([[1 & int(not y), 1 & y] for y in y_test], dtype='f')
+y_train = np.array([one_hot(y, N_CLASSES) for y in y_train], dtype='f')
+y_test  = np.array([one_hot(y, N_CLASSES) for y in y_test], dtype='f')
 
 x = tf.placeholder(tf.float32, [None, MAX_LEN])
-W = tf.Variable(tf.zeros([MAX_LEN, 2]))
-b = tf.Variable(tf.zeros([2]))
+W = tf.Variable(tf.zeros([MAX_LEN, N_CLASSES]))
+b = tf.Variable(tf.zeros([N_CLASSES]))
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-y_ = tf.placeholder(tf.float32, [None, 2])
+y = tf.matmul(x, W) + b
+y_ = tf.placeholder(tf.float32, [None, N_CLASSES])
 
-loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-trainer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+trainer = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
 correct_answer = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_answer, tf.float32))
@@ -44,16 +55,21 @@ with tf.Session() as sess:
 
     print 'Training...\n'
 
-    for i in range(1000):
-        x_batch, y_batch = gen_batch(x_train, y_train, 100)
+    for i in range(2000):
+        x_batch, y_batch = gen_batch(x_train, y_train, 500)
+        # x_batch, y_batch = mnist.train.next_batch(100)
+
+        # print x_batch.shape
+        # print y_batch[0]
 
         if i % 100 == 0:
             print '%d Loss: %f' % (i, sess.run(loss, feed_dict={x: x_batch, y_: y_batch}))
+            # print sess.run(y, feed_dict={x: [x_test[0]]})
 
         sess.run(trainer, feed_dict={x: x_batch, y_: y_batch})
 
     print
     print 'Loss: ', sess.run(loss, feed_dict={x: x_test, y_: y_test})
     print 'Accuracy: ', sess.run(accuracy, feed_dict={x: x_test, y_: y_test})
-    print sess.run(y, feed_dict={x: x_test[:2]})
-    print y_test[:2]
+    # print sess.run(y, feed_dict={x: x_test[:2]})
+    # print y_test[:2]
