3acf0450ee050b16285e06d0c828bb1908d9f526
Akshay Kashyap <AKK@zurich.ibm.com>
2017-07-31 10:48:31 +0200

    Removed sequence_length parameter.

--- rnn.py
+++ rnn.py
@@ -33,10 +33,10 @@ def attention(inputs, ATTENTION_SIZE):
 
     return output
 
-# Load Data
+# ----- Load Data -----
 (x_train, y_train), (x_test, y_test) = reuters.load_data(path="reuters.npz")
 
-# Pre-process
+# ----- Pre-process -----
 # MAX_LEN = max(len(max(x_train, key=len)), len(max(x_test, key=len)))
 MAX_LEN = 10
 N_CLASSES = 46
@@ -49,38 +49,42 @@ x_test = pad_sequences(x_test, MAX_LEN)
 y_train = np.array([one_hot(y, N_CLASSES) for y in y_train], dtype='f')
 y_test  = np.array([one_hot(y, N_CLASSES) for y in y_test], dtype='f')
 
-sequence_lengths = tf.placeholder(tf.int32, [None])
+# ----------
 
 x = tf.placeholder(tf.int32, [None, MAX_LEN])
 y_ = tf.placeholder(tf.float32, [None, N_CLASSES])
+# seq_lengths = tf.placeholder(tf.int32, [None])
 
-# Embedding Layer
+# ----- Embedding Layer -----
 # TODO: Switch to pre-trained embeddings
 embeddings_var = tf.Variable(tf.random_uniform([VOCAB_SIZE, 200], -1.0, 1.0), trainable=True)
 batch_embedded = tf.nn.embedding_lookup(embeddings_var, x)
 
 # ----- With Attention -----
 if len(sys.argv) > 1 and sys.argv[1] == '--attention':
-    # Bi-Directional GRU Cells
+    # + Bi-Directional GRU Cells +
     outputs, states = bidirectional_dynamic_rnn(
         GRUCell(HIDDEN_SIZE),
         GRUCell(HIDDEN_SIZE),
-        inputs=batch_embedded, sequence_length=sequence_lengths, dtype=tf.float32
+        inputs=batch_embedded, dtype=tf.float32
     )
 
     # + Attention Layer +
     attention_out = attention(outputs, ATTENTION_SIZE)
 
-    W = tf.Variable(tf.zeros([attention_out.get_shape()[1].value, N_CLASSES]))
+    out_shape = outputs.get_shape().as_list()
+
+    W = tf.Variable(tf.zeros([out_shape[1], N_CLASSES]))
     b = tf.Variable(tf.zeros([N_CLASSES]))
 
     y = tf.nn.softmax(tf.matmul(attention_out, W) + b)
 
 # ----- Without Attention -----
 else:
+    # + GRU RNN +
     outputs, states = dynamic_rnn(
         GRUCell(HIDDEN_SIZE),
-        inputs=batch_embedded, sequence_length=sequence_lengths, dtype=tf.float32
+        inputs=batch_embedded, dtype=tf.float32
     )
 
     out_shape = outputs.get_shape().as_list()
@@ -90,12 +94,18 @@ else:
 
     y = tf.nn.softmax(tf.matmul(tf.reshape(outputs, [-1, out_shape[2] * out_shape[1]]), W) + b)
 
+# ----------
+
 # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 trainer = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
+# ----------
+
 correct_answer = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_answer, tf.float32))
+
+# ----------
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -116,14 +126,14 @@ with tf.Session() as sess:
 
             x_batch, y_batch = x_train[start_index: end_index], y_train[start_index: end_index]
 
-            sequence_lengths = [len(x) for x in x_batch]
+            # seq_ls = np.array([len(x) for x in x_batch])
 
             if i_batch % 10 == 0:
                 print 'Batch %d | Loss: %f' % (i_batch, sess.run(loss,
-                    feed_dict={x: x_batch, y_: y_batch, sequence_lengths: sequence_lengths}))
+                    feed_dict={x: x_batch, y_: y_batch}))
 
             sess.run(trainer,
-                feed_dict={x: x_batch, y_: y_batch, sequence_lengths: sequence_lengths})
+                feed_dict={x: x_batch, y_: y_batch})
 
         print
 
